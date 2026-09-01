6bbe67a11d8209f48a602866f60f895f98012545
Ethan Almloff <76850177+EthanAlmloff@users.noreply.github.com>
2026-09-01 08:48:20 -0500

    Create ModernMinionBehaviorsBatch57Tests.cpp

---
+++ Tests/UnitTests/Battlegrounds/CardSets/ModernMinionBehaviorsBatch57Tests.cpp
@@ -0,0 +1,26 @@
+#include "doctest_proxy.hpp"
+#include <Rosetta/Battlegrounds/CardSets/ModernMinionBehaviorsBatch57.hpp>
+#include <Rosetta/Battlegrounds/Tasks/SimpleTasks/RandomCardToHandTask.hpp>
+
+using namespace RosettaStone::Battlegrounds;
+
+TEST_CASE("[Battlegrounds : ModernMinionBehaviorsBatch57] - Tad sell variants")
+{
+    std::map<std::string, CardDef> cards;
+    ModernMinionBehaviorsBatch57::AddAll(cards);
+    REQUIRE(cards.size() == 2);
+    CHECK(cards.contains("BG22_202"));
+    CHECK(cards.contains("BG22_202_G"));
+    const auto& normal = cards.at("BG22_202").power.GetTrigger().value();
+    const auto& golden = cards.at("BG22_202_G").power.GetTrigger().value();
+    CHECK(normal.GetTriggerType() == TriggerType::SELL_MINION);
+    CHECK(golden.GetTriggerType() == TriggerType::SELL_MINION);
+    const auto& normalTask = std::get<SimpleTasks::RandomCardToHandTask>(
+        normal.GetTasks().front());
+    const auto& goldenTask = std::get<SimpleTasks::RandomCardToHandTask>(
+        golden.GetTasks().front());
+    CHECK(normalTask.Amount() == 1);
+    CHECK(goldenTask.Amount() == 2);
+    CHECK(normalTask.RaceFilter() == Race::MURLOC);
+    CHECK(goldenTask.RaceFilter() == Race::MURLOC);
+}
